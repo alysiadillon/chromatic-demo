249ef4c7fe1cae5dc91c4eb9042894953a4631ee
Alysia Dillon <alysiadillon@Alysias-MBP.lan>
2022-05-26 19:27:48 -0700

    reverted to base setup

--- src/stories/Page.stories.js
+++ src/stories/Page.stories.js
@@ -20,8 +20,8 @@ export const LoggedOut = Template.bind({});
 
 // More on interaction testing: https://storybook.js.org/docs/vue/writing-tests/interaction-testing
 export const LoggedIn = Template.bind({});
-// LoggedIn.play = async ({ canvasElement }) => {
-//   const canvas = within(canvasElement);
-//   const loginButton = await canvas.getByText('Log in');
-//   await userEvent.click(loginButton);
-// };
+LoggedIn.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+  const loginButton = await canvas.getByText('Log in');
+  await userEvent.click(loginButton);
+};
